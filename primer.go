6425a536bc5f0fc937308500abd4828ba81b123f
Thanabodee <thanabodee@osdev.co.th>
2014-04-10 15:19:40 +0700

    refactor variable

--- primer.go
+++ primer.go
@@ -1,8 +1,7 @@
 package primer
 
 func PrimeFactor(n int) []int {
-	var factors []int = []int{}
-	var primeNumbers = []int{2, 3, 5, 7, 11, 13}
+	factors, primeNumbers := []int{}, []int{2, 3, 5, 7, 11, 13}
 	for index := 0; n != 1; index++ {
 		primeNumber := primeNumbers[index]
 		for ; n%primeNumber == 0; n /= primeNumber {
